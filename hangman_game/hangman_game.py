365a8e422042d56a8f90bf0689a79a3508a4166f
troches <troches@upper-link.com>
2023-10-16 12:21:29 +0200

    feat: hangman game

---
+++ hangman_game/hangman_game.py
@@ -0,0 +1,35 @@
+import random
+
+def two_player():
+    word = input("Player 1, enter the word to guess: ").lower()
+    hangman(word)
+    
+
+def single_player():
+    words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
+    word = random.choice(words)
+    hangman(word)
+
+def hangman(word):
+    guessed_letters = set()
+    tries = 6
+    score = 100
+
+    while tries > 0:
+        guessed_word = "".join([letter if letter in guessed_letters else "_" for letter in word])
+        print(guessed_word)
+        if guessed_word == word:
+            print("Congratulations! You guessed the word!")
+            return score / tries
+        guess = input("Player 2, guess a letter: ").lower()
+        if guess in guessed_letters:
+            print("You already guessed that letter!")
+        elif guess in word:
+            guessed_letters.add(guess)
+            print("Correct!")
+        else:
+            guessed_letters.add(guess)
+            tries -= 1
+            print(f"Incorrect! You have {tries} tries left.")
+    print(f"Game over! The word was {word}.")
+    return 0
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from fair_price_game import fair_price_game
+from hangman_game import hangman_game
+
+game_mode = {
+    0 : "Single player",
+    1 : "Two player",
+}
 
 def main():
     score = 0
@@ -6,6 +12,16 @@ def main():
     # fair score game 
     score = score + fair_price_game.guess_number()
 
+    # hangman's game
+    mode_choose = int(input("Choose a mode for hangman's game: \n 0. Two player \n 1. Single player \n"))
+    if(mode_choose not in game_mode.keys()):
+        print("Invalid mode choose, please try again!")
+        return main()
+    if(mode_choose == game_mode.single_player):
+        score = score + hangman_game.single_player()
+    else:
+        score = score + hangman_game.two_player()
+        
     # Other games here
 
     # Final score
